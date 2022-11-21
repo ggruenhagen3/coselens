b861ba179ba153b11735734ae8c476a2cf082fa2
ggruenhagen3 <ggruenhagen3@gatech.edu>
2022-11-21 17:32:10 -0500

    Changed to equal sign

--- R/coselens.R
+++ R/coselens.R
@@ -38,24 +38,24 @@ coselens = function(group1, group2, subset.genes.by = NULL, sequenced.genes = NU
   
     # Calculate dN/dS and the confidence intervals        
     message("[0] Running dndscv for group 1")
-    group1_dndsout <- coselens::dndscv(group1, gene_list = sequenced.genes, refdb = refdb, sm = sm, kc = kc, cv = cv, max_muts_per_gene_per_sample = max_muts_per_gene_per_sample, max_coding_muts_per_sample = max_coding_muts_per_sample, use_indel_sites = use_indel_sites, min_indels = min_indels, maxcovs = maxcovs, constrain_wnon_wspl = constrain_wnon_wspl, outp = outp, numcode = numcode, outmats=T, outmutrates = T, mingenecovs = mingenecovs)
+    group1_dndsout = coselens::dndscv(group1, gene_list = sequenced.genes, refdb = refdb, sm = sm, kc = kc, cv = cv, max_muts_per_gene_per_sample = max_muts_per_gene_per_sample, max_coding_muts_per_sample = max_coding_muts_per_sample, use_indel_sites = use_indel_sites, min_indels = min_indels, maxcovs = maxcovs, constrain_wnon_wspl = constrain_wnon_wspl, outp = outp, numcode = numcode, outmats=T, outmutrates = T, mingenecovs = mingenecovs)
     message("[0] Running dndscv for group 2")
-    group2_dndsout <- coselens::dndscv(group2, gene_list = sequenced.genes, refdb = refdb, sm = sm, kc = kc, cv = cv, max_muts_per_gene_per_sample = max_muts_per_gene_per_sample, max_coding_muts_per_sample = max_coding_muts_per_sample, use_indel_sites = use_indel_sites, min_indels = min_indels, maxcovs = maxcovs, constrain_wnon_wspl = constrain_wnon_wspl, outp = outp, numcode = numcode, outmats=T, outmutrates = T, mingenecovs = mingenecovs)
+    group2_dndsout = coselens::dndscv(group2, gene_list = sequenced.genes, refdb = refdb, sm = sm, kc = kc, cv = cv, max_muts_per_gene_per_sample = max_muts_per_gene_per_sample, max_coding_muts_per_sample = max_coding_muts_per_sample, use_indel_sites = use_indel_sites, min_indels = min_indels, maxcovs = maxcovs, constrain_wnon_wspl = constrain_wnon_wspl, outp = outp, numcode = numcode, outmats=T, outmutrates = T, mingenecovs = mingenecovs)
     
     # Calculate nonsynonymous mutation excess
-    group1_ex <- calc_ex(group1_dndsout)
-    group2_ex <- calc_ex(group2_dndsout)
-    group1_ex$ex_tot <- group1_ex[,2] + group1_ex[,3]
-    group2_ex$ex_tot <- group2_ex[,2] + group2_ex[,3]
-    ex_values <- merge(x=group1_ex[,c("gene_name","ex_mis","ex_non","ex_tot")], y=group2_ex[,c("gene_name", "ex_mis", "ex_non", "ex_tot")], by ="gene_name", suffixes=c(".group1", ".group2"))
+    group1_ex = calc_ex(group1_dndsout)
+    group2_ex = calc_ex(group2_dndsout)
+    group1_ex$ex_tot = group1_ex[,2] + group1_ex[,3]
+    group2_ex$ex_tot = group2_ex[,2] + group2_ex[,3]
+    ex_values = merge(x=group1_ex[,c("gene_name","ex_mis","ex_non","ex_tot")], y=group2_ex[,c("gene_name", "ex_mis", "ex_non", "ex_tot")], by ="gene_name", suffixes=c(".group1", ".group2"))
 
     # Calculate indel mutation excess
-    group1_ex_ind <- calc_ex_ind(group1_dndsout)
-    group2_ex_ind <- calc_ex_ind(group2_dndsout)
+    group1_ex_ind = calc_ex_ind(group1_dndsout)
+    group2_ex_ind = calc_ex_ind(group2_dndsout)
     group1_ex_ind$ex_ind = as.numeric(as.vector(group1_ex_ind$ex_ind))
     group2_ex_ind$ex_ind = as.numeric(as.vector(group2_ex_ind$ex_ind))
-    ex_ind_values <- merge(x=group1_ex_ind[,c("gene_name","ex_ind")], y=group2_ex_ind[,c("gene_name","ex_ind")], by="gene_name", suffixes=c(".group1",".group2"))
-    ex_values <- merge(x=ex_values, y=ex_ind_values, by="gene_name")
+    ex_ind_values = merge(x=group1_ex_ind[,c("gene_name","ex_ind")], y=group2_ex_ind[,c("gene_name","ex_ind")], by="gene_name", suffixes=c(".group1",".group2"))
+    ex_values = merge(x=ex_values, y=ex_ind_values, by="gene_name")
 
     # Put the data in the same order
     group1_sel_cv = group1_dndsout$sel_cv[order(as.numeric(rownames(group1_dndsout$sel_cv))),,drop=FALSE]
@@ -65,27 +65,27 @@ coselens = function(group1, group2, subset.genes.by = NULL, sequenced.genes = NU
     h0_sel_cv <<- lik_func(group1_dndsout, group2_dndsout)
 
     # Merge the data into one structure
-    lldf <- merge(x=h0_sel_cv[,c("gene_name", "llall", "llmis", "lltrunc", "llmis_check_A", "lltrunc_check_B", "llind0", "llind1")], y=group1_sel_cv[,c("gene_name", "wmis_cv", "wnon_cv", "llall")], by="gene_name", suffixes=c(".0", ".group1"))
-    lldf <- merge(x=lldf, y=group2_sel_cv[,c("gene_name", "wmis_cv", "wnon_cv", "llall")], by ="gene_name", suffixes=c(".group1", ".group2"))
-    colnames(lldf)[c(ncol(lldf)-2, ncol(lldf)-1, ncol(lldf))] <- c("wmis_cv.group2", "wnon_cv.group2", "llall.group2")
+    lldf = merge(x=h0_sel_cv[,c("gene_name", "llall", "llmis", "lltrunc", "llmis_check_A", "lltrunc_check_B", "llind0", "llind1")], y=group1_sel_cv[,c("gene_name", "wmis_cv", "wnon_cv", "llall")], by="gene_name", suffixes=c(".0", ".group1"))
+    lldf = merge(x=lldf, y=group2_sel_cv[,c("gene_name", "wmis_cv", "wnon_cv", "llall")], by ="gene_name", suffixes=c(".group1", ".group2"))
+    colnames(lldf)[c(ncol(lldf)-2, ncol(lldf)-1, ncol(lldf))] = c("wmis_cv.group2", "wnon_cv.group2", "llall.group2")
 
     # Alternative Hypothesis Log-Likelihoods for SNV
-    lldf$llall.1   <- lldf$llall.group1 + lldf$llall.group2
-    lldf <- lldf[,c(1, ncol(lldf), 3:ncol(lldf)-1)] # reorder llall.1 to the front
+    lldf$llall.1 = lldf$llall.group1 + lldf$llall.group2
+    lldf = lldf[,c(1, ncol(lldf), 3:ncol(lldf)-1)] # reorder llall.1 to the front
 
     # Optionally subset results by list of genes before p value calculation
     if (length(subset.genes.by) > 0) {
-      lldf      <- lldf[     which(lldf$gene_name      %in% subset.genes.by),]
-      ex_values <- ex_values[which(ex_values$gene_name %in% subset.genes.by),]
+      lldf      = lldf[     which(lldf$gene_name      %in% subset.genes.by),]
+      ex_values = ex_values[which(ex_values$gene_name %in% subset.genes.by),]
     }
 
     # Calculate p-values
-    lldf$pall    <- pchisq(- 2 * (lldf$llall.0 - lldf$llall.1), df=2, lower.tail = FALSE)
-    lldf$pmis    <- pchisq(- 2 * (lldf$llmis   - lldf$llall.1), df=1, lower.tail = FALSE)
-    lldf$ptrunc  <- pchisq(- 2 * (lldf$lltrunc - lldf$llall.1), df=1, lower.tail = FALSE)
-    lldf$pind    <- 1 - pchisq(2*(lldf$llind1 - lldf$llind0),df=1)
-    lldf$pglobal <- 1 - pchisq(-2 * (log(lldf$pall) + log(lldf$pind)), df = 4)
-    lldf <- lldf[order(lldf$pglobal),] # order data by p-values
+    lldf$pall    = pchisq(- 2 * (lldf$llall.0 - lldf$llall.1), df=2, lower.tail = FALSE)
+    lldf$pmis    = pchisq(- 2 * (lldf$llmis   - lldf$llall.1), df=1, lower.tail = FALSE)
+    lldf$ptrunc  = pchisq(- 2 * (lldf$lltrunc - lldf$llall.1), df=1, lower.tail = FALSE)
+    lldf$pind    = 1 - pchisq(2*(lldf$llind1 - lldf$llind0),df=1)
+    lldf$pglobal = 1 - pchisq(-2 * (log(lldf$pall) + log(lldf$pind)), df = 4)
+    lldf = lldf[order(lldf$pglobal),] # order data by p-values
 
     # Adjust p-values
     lldf$qind = p.adjust(lldf$pind, method = "BH")
@@ -95,7 +95,7 @@ coselens = function(group1, group2, subset.genes.by = NULL, sequenced.genes = NU
     lldf$qglobal = p.adjust(lldf$pglobal, method = "BH")
 
     # Add mutation excess data
-    lldf <- merge(x = lldf, y = ex_values[,c("gene_name","ex_tot.group1","ex_tot.group2", "ex_ind.group1", "ex_ind.group2", "ex_mis.group1", "ex_mis.group2", "ex_non.group1", "ex_non.group2")], by = "gene_name")
+    lldf = merge(x = lldf, y = ex_values[,c("gene_name","ex_tot.group1","ex_tot.group2", "ex_ind.group1", "ex_ind.group2", "ex_mis.group1", "ex_mis.group2", "ex_non.group1", "ex_non.group2")], by = "gene_name")
 
     # Single Group Tests
     single.test.names = c("psub.group", "pind.group", "pmis.group", "ptrunc.group", "pglobal.group", "qsub.group", "qind.group", "qmis.group", "qtrunc.group", "qglobal.group")
